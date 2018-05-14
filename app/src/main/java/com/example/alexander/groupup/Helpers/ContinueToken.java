f038775982468e4969d7908e498e7d1950117201
Fabio Moretti <fmoretti981@gmail.com>
2018-05-14 17:28:09 +0200

    Deleted all for gitignore

--- app/src/main/java/com/example/alexander/groupup/Helpers/ContinueToken.java
+++
@@ -1,6 +0,0 @@
-package com.example.alexander.groupup.Helpers;
-
-public class ContinueToken
-{
-    public boolean CanContinue = false;
-}
--- app/src/main/java/com/example/alexander/groupup/Helpers/OnGetResultListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.alexander.groupup.Helpers;
-
-public interface OnGetResultListener<T> {
-    void OnSuccess(T value);
-    void OnFail();
-}
